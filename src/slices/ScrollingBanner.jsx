2bf2907aa49cae9df01c04bafcbdb6ea26d2d3fb
Hunter Mann <hunter.mann24@gmail.com>
2025-06-21 16:25:21 -0400

    adding fixed scroll banner

--- src/slices/ScrollingBanner.jsx
+++ src/slices/ScrollingBanner.jsx
@@ -7,7 +7,7 @@ import { useInView } from 'react-intersection-observer'
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import React, { forwardRef, useEffect } from "react";
+import React, { forwardRef, useEffect, useRef } from "react";
 
 
 
@@ -26,7 +26,22 @@ const ScrollingBanner = () => {
 
 
 
+    const liRefs = useRef([])
+    liRefs.current = [];
 
+    const addToRefs = (el) => {
+        if (el && !liRefs.current.includes(el)) {
+            liRefs.current.push(el)
+        }
+    }
+
+    const handleMouseEnter = (index) => {
+        gsap.to(liRefs.current[index], {
+            opacity: 1,
+            
+        })
+        console.log('I am hovered')
+    }
     const { ref , inView , entry } = useInView({
         triggerOnce: true,
         threshold: 0.5
@@ -39,79 +54,148 @@ const ScrollingBanner = () => {
             gsap.to('#quote', {opacity: 1});
 
         }, [inView])
+
+    useEffect(() => {
+        
+    })
     
     return(
 
         <>
-
-        {/* 
-      <SocialPanel realCount={100} />
-      <section className="banner-container">
-        <div className="banner">
-          <div className="banner-content">
-            <div className="flex gap-3 items-center">
-            <span>Get Lucky!</span>
-            <FontAwesomeIcon className="text-green-600" icon={faClover} />
-
-
-            </div>
-            <div>
-              <span>Try our Honey Gold Flavor</span>
-            </div>
-          </div>
-
-                                                      
-        </div>
-      </section>
-
-      <Newsletter /> */}
+     
       <section className="lg:mt-[46px] mt-[57px] w-[100vw] max-w-[100%] overflow-x-clip">
 
             <div className="lg:px-[50px] px-[60px] relative">
-                <h1 className="text-center font-juju lg:mb-[488px] mb-[47px] mt-[60px] lg:mt-[50px] whitespace-pre-line">Get Lucky
+                <h1 id="scroll-header" className="text-center font-juju lg:mb-[488px] mb-[47px] mt-[60px] lg:mt-[50px] text-green-500 whitespace-pre-line">Get Lucky
                 </h1>
                 <div className=" lg:w-[832px] lg:h-[280px] h-[366px]"></div>
                 <div className="absoulte left-0 right-0 lg:bottom-[85px] bottom-0 lg:h-[628px] h-[366px]">
                     <div className="w-[100dvw] overflow-hidden">
                         <div className="flex flex-row overflow-hidden" draggable="false" id="scroll-banner">
-                            <div id="scroll-content" className="flex flex-row gap-3">
-                                <div className="lg:mr-[80px] mr-[30px] lg:mt-[calc(calc(var(--mtr)*60px)+60px)] mt-[calc(calc(var(--mtr)*20px)+20px)]">
-                                    <div id="item-scroll" className="lg:w-[146px] w-[97px] aspect-square border border-green-500 rounded-full drop-shadow-esg bg-blue-500 bg-opacity-60 flex flex-col items-center justify-center ">
-                                        <span>
-                                            Flavor Focused
+                            <div  id="scroll-content" className="flex flex-row gap-3">
+                              <ul>
+                                <div>
+                                    <li onMouseEnter={() => handleMouseEnter(0)}  className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            Flavor-Focused
                                         </span>
-                                    </div>
+                                        <span key={0} ref={addToRefs} className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Our tea puts an emphasis on flavor!
+                                        </span>
+                                    </li>
                                     <div className="vertical-line"></div>
-
-                                    <div className=" w-[2px] lg:h-[374px] h-[200px] mx-auto lg:mt-[22px] mt-[5px]"></div>
                                 </div>
-                                <div className="lg:mr-[80px] mr-[30px] lg:mt-[calc(calc(var(--mtr)*60px)+60px)] mt-[calc(calc(var(--mtr)*20px)+20px)]">
-                                    <div id="item-scroll" className="lg:w-[146px] w-[97px] aspect-square border rounded-full drop-shadow-esg bg-blue-500 bg-opacity-60 flex flex-col items-center justify-center ">
-                                        <span>
-Community-Driven                                        </span>
-                                    </div>
-                                     <div className="vertical-line"></div>
-
-                                    <div className=" w-[2px] lg:h-[374px] h-[200px] mx-auto lg:mt-[22px] mt-[5px]"></div>
-                                </div>
-                                <div className="lg:mr-[80px] mr-[30px] lg:mt-[calc(calc(var(--mtr)*60px)+60px)] mt-[calc(calc(var(--mtr)*20px)+20px)]">
-                                    <div id="item-scroll" className="lg:w-[146px] w-[105px] aspect-square border border-green-500 rounded-full drop-shadow-esg bg-blue-500 bg-opacity-60 flex flex-col items-center justify-center ">
-                                        <span>
-100% Real Tea                                        </span>
-                                    </div>
+                                <div>
+                                    <li className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            Community-Driven
+                                        </span>
+                                        <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Lucky cares about the community.
+                                        </span>
+                                    </li>
                                     <div className="vertical-line"></div>
-
-                                    <div className=" w-[2px] lg:h-[374px] h-[200px] mx-auto lg:mt-[22px] mt-[5px]"></div>
                                 </div>
-                                <div className="lg:mr-[80px] mr-[30px] lg:mt-[calc(calc(var(--mtr)*60px)+60px)] mt-[calc(calc(var(--mtr)*20px)+20px)]">
-                                    <div id="item-scroll" className="lg:w-[146px] w-[97px] aspect-square border border-green-500 rounded-full drop-shadow-esg bg-blue-500 bg-opacity-60 flex flex-col items-center justify-center ">
-                                        <span className="text-white">
-Locally-Sourced                                        </span>
-                                    </div>
+                                <div>
+                                    <li className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            100% Real Tea
+                                        </span>
+                                        <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Tea brewed with authentic green & black tea leaves
+                                        </span>
+                                    </li>
                                     <div className="vertical-line"></div>
-
-                                    <div className=" w-[2px] lg:h-[374px] h-[200px] mx-auto lg:mt-[22px] mt-[5px]"></div>
                                 </div>
+                                <div><li className="flex flex-col items-center">
+                                    <span id="descriptive-text-heading">
+                                        Locally-Sourced Honey
+                                    </span>
+                                    <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                        Honey used right from the backyard of Dallas, Texas
+                                    </span>
+                                </li>
+                                <div className="vertical-line"></div>
+                                </div>
+                                 <div><li className="flex flex-col items-center">
+                                    <span id="descriptive-text-heading">
+                                        % less sugar than Arizona
+                                    </span>
+                                    <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                        Sugar contained in Lucky products are less than other leading brands
+                                    </span>
+                                </li>
+                                <div className="vertical-line"></div>
+                                </div>
+                                    
+                                
+                                 
+                                 
+                                 
+                                 
+                              </ul>
+                              <ul aria-hidden="true">
+                                <div>
+                                    <li className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            Flavor-Focused
+                                        </span>
+                                        <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Our tea puts an emphasis on flavor!
+                                        </span>
+                                    </li>
+                                    <div className="vertical-line"></div>
+                                </div>
+                                <div>
+                                    <li className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            Community-Driven
+                                        </span>
+                                        <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Lucky cares about the community.
+                                        </span>
+                                    </li>
+                                    <div className="vertical-line"></div>
+                                </div>
+                                <div>
+                                    <li className="flex flex-col items-center">
+                                        <span id="descriptive-text-heading">
+                                            100% Real Tea
+                                        </span>
+                                        <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                            Tea brewed with authentic green & black tea leaves
+                                        </span>
+                                    </li>
+                                    <div className="vertical-line"></div>
+                                </div>
+                                <div><li className="flex flex-col items-center">
+                                    <span id="descriptive-text-heading">
+                                        Locally-Sourced Honey
+                                    </span>
+                                    <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                        Honey used right from the backyard of Dallas, Texas
+                                    </span>
+                                </li>
+                                <div className="vertical-line"></div>
+                                </div>
+                                 <div><li className="flex flex-col items-center">
+                                    <span id="descriptive-text-heading">
+                                        % less sugar than Arizona
+                                    </span>
+                                    <span className="text-sm min-w-[30px] border" id="descriptive-text-caption">
+                                        Sugar contained in Lucky products are less than other leading brands
+                                    </span>
+                                </li>
+                                <div className="vertical-line"></div>
+                                </div>
+                                    
+                                
+                                 
+                                 
+                                 
+                                 
+                              </ul>
+                               
                                 
                             </div>
                             
